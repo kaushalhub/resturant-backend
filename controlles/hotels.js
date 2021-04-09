bd03995f93f96c5e28935be7d7981591fdb2337f
kaushalhub <jainkaushal123465@gmail.com>
2021-04-09 14:52:35 +0530

    class-5

--- controlles/hotels.js
+++ controlles/hotels.js
@@ -3,30 +3,56 @@ const Hotel = require('../model/Hotel');
 // @desc   get All Hotels
 // url(get)     /api/v1/hotels
 // Type    Public
-exports.getHotels = (req, res, next) => {  
-  res.send({ success: true, msg: "Hotel Fetch SuccessFully!" }).json();
+exports.getHotels = async (req, res, next) => {  
+  try {
+    const hotels = await Hotel.find();
+    console.log(hotels)
+    res.status(200).json({ success : true, data : hotels })
+  } catch (err) {
+    res.status(400).json({ success : false })
+  }
 };
 
 // @desc   add Hotels
 // url(post)     /api/v1/hotels
 // Type    Public
-exports.addHotels = async (req, res, next) => {
-  const hotel = await Hotel.create(req.body);
+exports.addHotels = async(req, res, next) => {
+  try {
+    const hotel = await Hotel.create(req.body);
+
+    res.status(201).json({ success : true, data : hotel })
+  } catch (err) {
+    res.status(400).json({ success : false })
+  }
 };
 
 // @desc   fetch Single Hotels
 // url(get)     /api/v1/hotels/:id
 // Type    Public
 exports.getHotel = async (req, res, next) => {
-  res
-    .send({ success: true, msg: `Single Hotel Fetch By ${req.params.id}` })
-    .json();
+  try{
+    const hotel = await Hotel.findById(req.params.id);
+
+    res.status(200).json({ success : true, data : hotel })
+  } catch (err) {
+    res.status(400).json({ success : false })
+  }
 };
 
 // @desc    update Hotels
 // url(put)     /api/v1/hotels/:id
 // Type    Public
-exports.updateHotel = (req, res, next) => {
+exports.updateHotel = async (req, res, next) => {
+  try {
+    const hotel = await Hotel.findByIdAndUpdate(req.params.id, req.body, {
+      new : true,
+      runValidators : true
+    })
+
+    res.status(200).json({ success : true, data : hotel })
+  } catch(err) {
+    res.status(400).json({ success : false })
+  }
   res.send({ success: true, msg: `Update Hotel ${req.params.id}` }).json();
 };
 
@@ -34,5 +60,11 @@ exports.updateHotel = (req, res, next) => {
 // url(delete)     /api/v1/hotels/:id
 // Type    Public
 exports.deleteHotel = async (req, res, next) => {
-  res.send({ success: true, msg: `Delete Hotel ofId ${req.params.id}` }).json();
+  try {
+    const hotel = await Hotel.findByIdAndRemove(req.params.id)
+    res.status(200).json({ success : true, data : {} })
+
+  } catch (err) {
+    res.status(400).json({ success : false })
+  }
 };
